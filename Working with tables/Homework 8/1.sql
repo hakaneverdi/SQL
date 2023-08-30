f45439e5c2ced63a5345d5f0a3313046d826dcce
Hakan Everdi <hknevrdi@gmail.com>
2023-08-30 22:51:07 +0300

    homeworks

---
+++ Working with tables/Homework 8/1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	birhtday DATE, 
+	email VARCHAR(100)
+	);
---
+++ Working with tables/Homework 8/4.sql
@@ -0,0 +1,5 @@
+DELETE FROM employee WHERE id = 10;
+DELETE FROM employee WHERE name = 'Ahmet';
+DELETE FROM employee WHERE birthday = '1995-07-07';
+DELETE FROM employee WHERE email = 'delete_me@example.com';
+DELETE FROM employee WHERE id = 15;
