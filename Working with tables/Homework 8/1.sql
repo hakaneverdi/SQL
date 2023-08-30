bdf6856f4e172cd3fed67e13aeea622d7614a2a3
Hakan Everdi <hknevrdi@gmail.com>
2023-08-31 01:01:43 +0300

    Delete Working with tables/Homework 8 directory

--- Working with tables/Homework 8/1.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE employee (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(50),
-	birhtday DATE, 
-	email VARCHAR(100)
-	);
--- Working with tables/Homework 8/4.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM employee WHERE id = 10;
-DELETE FROM employee WHERE name = 'Ahmet';
-DELETE FROM employee WHERE birthday = '1995-07-07';
-DELETE FROM employee WHERE email = 'delete_me@example.com';
-DELETE FROM employee WHERE id = 15;
